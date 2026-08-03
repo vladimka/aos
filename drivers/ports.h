b1b16519157b9c54e3f737b0ae3b8d51feb6601c
vladimka <zverevv.04@gmail.com>
2026-08-03 15:54:59 +0800

    add inw/inl/outl port I/O helpers

--- drivers/ports.h
+++ drivers/ports.h
@@ -15,4 +15,20 @@ static inline void outw(unsigned short port, unsigned short data) {
     __asm__ volatile("out %0, %1" : : "a"(data), "Nd"(port));
 }
 
+static inline unsigned short inw(unsigned short port) {
+    unsigned short result;
+    __asm__ volatile("in %1, %0" : "=a"(result) : "Nd"(port));
+    return result;
+}
+
+static inline unsigned int inl(unsigned short port) {
+    unsigned int result;
+    __asm__ volatile("in %1, %0" : "=a"(result) : "Nd"(port));
+    return result;
+}
+
+static inline void outl(unsigned short port, unsigned int data) {
+    __asm__ volatile("out %0, %1" : : "a"(data), "Nd"(port));
+}
+
 #endif
